1f6ee3e0cd1788c50824f2972af3f07156e9df7e
Nenad Novitovic <novitovic.nenad@gmail.com>
2022-04-14 12:01:40 +0200

    Linked login with database

--- webapp/src/main/java/engineering/it/web/webapp/action/login/LoginAction.java
+++ webapp/src/main/java/engineering/it/web/webapp/action/login/LoginAction.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import engineering.it.web.webapp.action.AbstractAction;
 import engineering.it.web.webapp.constant.WebConstant;
 import engineering.it.web.webapp.domain.User;
+import engineering.it.web.webapp.persistence.MyEntityManagerFactory;
 
 public class LoginAction extends AbstractAction{
 
@@ -21,6 +22,7 @@ public class LoginAction extends AbstractAction{
 				@SuppressWarnings("unchecked")
 				List<User> loggedInUsers = (List<User>)request.getServletContext().getAttribute("loggedInUsers");
 				loggedInUsers.add(user);
+				request.setAttribute("user", user);
 				return WebConstant.PAGE_HOME;
 			}else {
 				request.setAttribute("error", "User with that username is already logged in!");
@@ -34,8 +36,10 @@ public class LoginAction extends AbstractAction{
 
 	private User login(HttpServletRequest request) {
 		List<User> users = new ArrayList<User>(); // pozovi userStorage i vrati listu usera
-		users.add(new User("admin","admin","admin"));
+		//users.add(new User("admin","admin","admin"));
+		users = MyEntityManagerFactory.getEntityManagerFactory().createEntityManager().createQuery("from User", User.class).getResultList();
 		User user = null;
+		System.out.println(users);
 		
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
@@ -60,9 +64,7 @@ public class LoginAction extends AbstractAction{
 			if(u.getUsername().equals(user.getUsername()))
 				loggedIn=true;
 		}
-		
 		return loggedIn;
-		
 	}
 
 }
